bcc437fcc7dc9d2ffe85eeefc78c98d69e18979f
Advait <advaitbansode4@gmail.com>
2021-05-07 02:11:12 +0530

    Changed folder structure

--- Websocket/websocket/android/app/src/main/kotlin/com/example/websocket/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.websocket
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Websocket/websocket/lib/constants.dart
+++
@@ -1,3 +0,0 @@
-const YOUR_SERVER_IP = 'localhost';
-const YOUR_SERVER_PORT = '3000';
-const URL = 'ws://$YOUR_SERVER_IP:$YOUR_SERVER_PORT';
