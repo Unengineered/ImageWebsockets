cf3d828f8d20b398bcc3d4978f093879e5d33bf8
vaidehibhagwat <vaidehibhagwat225@gmail.com>
2021-05-07 18:08:48 +0530

    image received with meta data

--- backend_image_websocket/server.js
+++ backend_image_websocket/server.js
@@ -13,28 +13,47 @@ app.use('/uploads', express.static(__dirname + '/uploads'))
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
-app.get('/upload',(req,res)=>{
-    res.sendFile(__dirname+"/browser.html")
+app.get('/upload', (req, res) => {
+    res.sendFile(__dirname + "/browser.html")
 })
+
+//to convert string to arrayBuffer
+function str2ab(str) {
+    var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+    var bufView = new Uint8Array(buf);
+    for (var i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
 
 wss.on('connection', (ws) => {
     //connection is up, let's add a simple simple event
     ws.on('message', (message) => {
-        let data =  new Buffer.from(message);
-        fs.writeFile('./uploads/'+ data.length+'.png', data, 'binary', function (err) {
-                if (err) {
-                    console.log("error")
-                }
-                else {
-                    console.log("done")
-                }
+        let data = JSON.parse(message)
+        let image = new Buffer.from(str2ab(data.ab));
+        fs.writeFile('./uploads/' + data.fileName, image, 'binary', function (err) {
+            if (err) {
+                console.log("error")
             }
+            else {
+                console.log("done")
+            }
+        }
         )
+
+
+        let fileInfo = {
+            fileName: data.fileName,
+            fileSize: data.fileSize,
+            fileType: data.fileType
+        }
+        console.log(fileInfo)
         //log the received message and send it back to the client
-        //console.log('received: %s', message);
-        for(var cl of wss.clients) {
+        console.log(`received:${data.fileName}`);
+        for (var cl of wss.clients) {
             cl.send(message);
-          }
+        }
     });
 
     //send immediatly a feedback to the incoming connection    
